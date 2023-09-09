5476e26ffa1c27096ffbd0aa11e817e52a337358
Sylvia Jacoby <sylvia.jacoby@duke.edu>
2023-09-09 14:30:58 -0400

    insert files into MongoDB

--- db.py
+++ db.py
@@ -1,6 +1,8 @@
 import json
 import os
+import glob
 
+# Connect Python file to MongoDB
 from pymongo.mongo_client import MongoClient
 from pymongo.server_api import ServerApi
 uri = "mongodb+srv://sylviajacoby:Ellaella$2@cluster0.3cfxdom.mongodb.net/?retryWrites=true&w=majority"
@@ -13,16 +15,23 @@ try:
 except Exception as e:
     print(e)
 
+# Connect Python to a specific database
+from pymongo.database import Database
+db = client["HackDuke"]
 
 # Define the directory containing your text files
-text_files_directory = "/path/to/your/text/files"
+text_files_directory = '/users/sylviajacoby/Documents/fortheprogram/textfiles'
+print(text_files_directory)
+
+# Delete all existing documents in the collection
+db["documents"].delete_many({})
 
 # Iterate over the files in the directory
-for filename in os.listdir(text_files_directory):
-    if filename.endswith(".txt"):  # Assuming the files have a .txt extension
-        with open(os.path.join(text_files_directory, filename), "r") as file:
-            content = file.read()
+for filename in glob.glob(f"{text_files_directory}/*.txt"):
+    print(filename)
+    with open(filename, "r") as file:
+        content = file.read()
+        # Insert content into MongoDB collection
+        db["documents"].insert_one({"filename": filename, "content": content})
 
-            # Insert content into MongoDB collection
-            db["your-collection-name"].insert_one({"filename": filename, "content": content})
 
